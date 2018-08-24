8e1aa474812b18278bba1a9d14cd9c6c49fddd44
Diego Benavides <diegob41@gmail.com>
2018-08-24 11:39:47 -0600

    Added MockSystemClock class to aid in testing

---
+++ mocks/MockSystemClock.cpp
@@ -0,0 +1,12 @@
+/*
+ * MockSystemClock.cpp
+ *
+ *  Created on: Aug 24, 2018
+ *      Author: diebm
+ */
+
+#include "MockSystemClock.h"
+
+namespace ranetos {
+
+} /* namespace ranetos */
---
+++ mocks/MockSystemClock.h
@@ -0,0 +1,38 @@
+/*
+ * MockSystemClock.h
+ *
+ *  Created on: Aug 24, 2018
+ *      Author: diebm
+ */
+
+#ifndef MOCKS_MOCKSYSTEMCLOCK_H_
+#define MOCKS_MOCKSYSTEMCLOCK_H_
+
+#include <ISystemClock.h>
+
+namespace ranetos {
+
+class MockSystemClock: public ranetos::ISystemClock {
+	unsigned long millisecondsValue;
+public:
+	MockSystemClock() {
+		millisecondsValue = 0;
+	}
+	virtual ~MockSystemClock() {}
+
+	virtual unsigned long milliseconds() {
+		return getMillisecondsValue();
+	}
+
+	unsigned long getMillisecondsValue() const {
+		return millisecondsValue;
+	}
+
+	void setMillisecondsValue(unsigned long millisecondsValue) {
+		this->millisecondsValue = millisecondsValue;
+	}
+};
+
+} /* namespace ranetos */
+
+#endif /* MOCKS_MOCKSYSTEMCLOCK_H_ */
